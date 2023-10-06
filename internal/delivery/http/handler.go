40a26b2a6a73b035851575b931ac0c645ead7ac1
Andrii <75880958+andriimwks@users.noreply.github.com>
2023-10-06 18:44:14 +0300

    Move user handlers to a separate file

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -1,11 +1,8 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/andriimwks/go-fiber-template/internal/config"
 	"github.com/andriimwks/go-fiber-template/internal/service"
-	"github.com/andriimwks/go-fiber-template/pkg/templatetags"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -18,80 +15,5 @@ type handler struct {
 func Init(app *fiber.App, cfg *config.Config, services *service.Services) {
 	h := handler{app, cfg, services}
 	h.app.Use(h.authMiddleware)
-
-	h.app.Get("/", h.indexGET)
-	h.app.Get("/signin", h.signInGET)
-	h.app.Post("/signin", h.signInPOST)
-	h.app.Get("/signup", h.signUpGET)
-	h.app.Post("/signup", h.signUpPOST)
-	h.app.Get("/signout", h.signOutGET)
-
-	templatetags.AddURL("index", "/")
-	templatetags.AddURL("user:signin", "/signin")
-	templatetags.AddURL("user:signup", "/signup")
-	templatetags.AddURL("user:signout", "/signout")
-}
-
-func (handler) indexGET(c *fiber.Ctx) error {
-	return c.Render("pages/index", fiber.Map{})
-}
-
-func (handler) signInGET(c *fiber.Ctx) error {
-	if c.Locals("user") != nil {
-		return c.Redirect(templatetags.GetURL("index"))
-	}
-	return c.Render("pages/signin", fiber.Map{})
-}
-
-func (h *handler) signInPOST(c *fiber.Ctx) error {
-	if c.Locals("user") != nil {
-		return c.Redirect(templatetags.GetURL("index"))
-	}
-
-	var in service.SignInInput
-	if err := c.BodyParser(&in); err != nil {
-		return c.SendStatus(http.StatusBadRequest)
-	}
-
-	tp, err := h.services.Users.SignIn(in)
-	if err != nil {
-		return c.Render("pages/signin", fiber.Map{"errors": []string{err.Error()}})
-	}
-
-	c.Cookie(&fiber.Cookie{Name: "ACCESS_TOKEN", Value: tp.AccessToken, Secure: true, HTTPOnly: true})
-	c.Cookie(&fiber.Cookie{Name: "REFRESH_TOKEN", Value: tp.RefreshToken, Secure: true, HTTPOnly: true})
-	return c.Redirect(templatetags.GetURL("index"))
-}
-
-func (handler) signUpGET(c *fiber.Ctx) error {
-	if c.Locals("user") != nil {
-		return c.Redirect(templatetags.GetURL("index"))
-	}
-	return c.Render("pages/signup", fiber.Map{})
-}
-
-func (h *handler) signUpPOST(c *fiber.Ctx) error {
-	if c.Locals("user") != nil {
-		return c.Redirect(templatetags.GetURL("index"))
-	}
-
-	var in service.SignUpInput
-	if err := c.BodyParser(&in); err != nil {
-		return c.SendStatus(http.StatusBadRequest)
-	}
-
-	tp, err := h.services.Users.SignUp(in)
-	if err != nil {
-
-		return c.Render("pages/signup", fiber.Map{"errors": []string{err.Error()}})
-	}
-
-	c.Cookie(&fiber.Cookie{Name: "ACCESS_TOKEN", Value: tp.AccessToken, Secure: true, HTTPOnly: true})
-	c.Cookie(&fiber.Cookie{Name: "REFRESH_TOKEN", Value: tp.RefreshToken, Secure: true, HTTPOnly: true})
-	return c.Redirect(templatetags.GetURL("index"))
-}
-
-func (handler) signOutGET(c *fiber.Ctx) error {
-	c.ClearCookie("ACCESS_TOKEN", "REFRESH_TOKEN")
-	return c.Redirect(templatetags.GetURL("index"))
+	h.initUserHandlers()
 }
